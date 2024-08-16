f2b77f247b552bc0b4ee217f35e73cc0cb5e0bf6
Louis <pukpuk.tw@gmail.com>
2024-08-17 01:17:07 +0800

    feat: add initial project structure and update site title and description

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '老鹿寫點東西';
+export const SITE_DESCRIPTION = '歡迎來到老鹿的知識平台';
---
+++ src/env.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+  readonly BUILDER_API_PUBLIC_KEY: string;
+}
