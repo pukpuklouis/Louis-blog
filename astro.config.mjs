b5674c778b7680e6a91d3f25f3cf46e08d970d11
Louis <pukpuk.tw@gmail.com>
2025-03-07 17:52:51 +0800

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,13 @@ import react from "@astrojs/react";
 import icon from "astro-icon";
 import pageInsight from "astro-page-insight";
 import metaTags from "astro-meta-tags";
-
+import partytown from "@astrojs/partytown";
 import tailwindConfigViewer from "astro-tailwind-config-viewer";
+
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blogs.itslouis.cc',
-  integrations: [mdx(), sitemap(), tailwind(), react(), icon(), pageInsight(), metaTags(), tailwindConfigViewer()]
+  integrations: [mdx(), sitemap(), tailwind(), react(), icon(), pageInsight(), metaTags(), tailwindConfigViewer(), partytown()]
 });
