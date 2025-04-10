2d9a3d2f9784ea7302d617705fbce497476f5bc3
Louis <pukpuk.tw@gmail.com>
2025-04-10 21:48:39 +0800

    update sitemap configuration

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,15 +1,13 @@
-import type { APIRoute } from 'astro';
+import type { APIRoute } from "astro";
 
 const getRobotsTxt = (sitemapURL: URL) => `
 User-agent: *
 Allow: /
 
 Sitemap: ${sitemapURL.href}
-
-crawl-delay: 10
 `;
 
 export const GET: APIRoute = ({ site }) => {
-  const sitemapURL = new URL('sitemap-index.xml', site);
+  const sitemapURL = new URL("sitemap-index.xml", site);
   return new Response(getRobotsTxt(sitemapURL));
 };
