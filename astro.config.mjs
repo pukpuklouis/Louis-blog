5f324b6f90e9408d16960f363394a79f222f6c0d
Louis <pukpuk.tw@gmail.com>
2025-04-24 19:56:34 +0800

    Add HubSpot tracking code and configure Partytown forwarding

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,7 @@ export default defineConfig({
     partytown({
       config: {
         debug: false,
-        forward: ["dataLayer.push", "h8OQRUhXvvY2LPkuL7o0IQ"],
+        forward: ["dataLayer.push", "h8OQRUhXvvY2LPkuL7o0IQ", "hbspt"],
         proxies: [
           {
             host: "analytics.ahrefs.com",
